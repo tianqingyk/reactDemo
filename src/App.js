743a7c5936b4a123020552e30aea15bb7b86ec96
tianqingyk <734743339@qq.com>
2023-04-20 03:30:21 -0400

    test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css'
 import React, { Component } from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom'
 import HomePage from './pages/HomePage/HomePage'
 import Header from './component/Header/Header'
 import Footer from './component/Footer/Footer'
@@ -8,6 +8,7 @@ import Contact from './pages/Contact/Contact'
 import Death from './pages/Death/Death'
 import Deconstruction from './pages/Deconstruction/Deconstruction'
 import Salome from './pages/Salome/Salome'
+import Navbar from './component/Navbar/Navbar'
 
 
 class App extends Component {
@@ -17,13 +18,14 @@ class App extends Component {
         <Header />
         <Router>
           <Routes>
-            <Route path="/" exact Component={HomePage} />
+            <Route path="/" Component={HomePage} />
             <Route path="/contact" Component={Contact} />
             <Route path="/death" Component={Death} />
             <Route path="/deconstruction" Component={Deconstruction} />
             <Route path="/salome" Component={Salome} />
           </Routes>
         </Router>
+        <Navbar />
         <Footer />
       </div>
     )
